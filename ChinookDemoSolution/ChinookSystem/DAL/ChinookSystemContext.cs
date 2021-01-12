fa139b35ac1002845240885c962a6b8e49d19fd0
aolthoff2 <70964419+aolthoff2@users.noreply.github.com>
2021-01-12 16:13:53 -0700

    Setup of DAL, web config and connection strings.

---
+++ ChinookDemoSolution/ChinookSystem/DAL/ChinookSystemContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region ADDITIONAL NAMESPACES
+using System.Data.Entity;
+using ChinookSystem.ENTITIES;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    internal class ChinookSystemContext:DbContext
+    {
+        public ChinookSystemContext() : base("name=ChinookDB")
+        {
+
+        }
+
+        public DbSet<ARTIST> ARTISTs { get; set; }
+        public DbSet<ALBUM> ALBUMs { get; set; }
+        public DbSet<GENRE> GENREs { get; set; }
+        public DbSet<MEDIATYPE> MEDIATYPEs { get; set; }
+        public DbSet<TRACK> TRACKs { get; set; }
+    }
+}
